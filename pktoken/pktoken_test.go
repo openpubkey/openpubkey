fbb0e0d1af482ca6ced9b635c894748aa73a2809
lgmugnier <mugnier@bu.edu>
2023-10-12 09:07:49 -0400

    Revert "add simple cli"
    This reverts commit 65010802d3f475d6bb7bea8c8b0f19f93262e394.

--- pktoken/pktoken_test.go
+++ pktoken/pktoken_test.go
@@ -2,8 +2,12 @@ package pktoken_test
 
 import (
 	"testing"
+	"time"
 
-	"github.com/openpubkey/openpubkey/pktoken/mocks"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"github.com/lestrrat-go/jwx/v2/jwt/openid"
+	"github.com/openpubkey/openpubkey/pktoken"
+	"github.com/openpubkey/openpubkey/pktoken/clientinstance"
 	"github.com/openpubkey/openpubkey/signer"
 )
 
@@ -13,7 +17,7 @@ func TestPkToken(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	pkt, err := mocks.GenerateMockPKToken(signer)
+	pkt, err := createPKToken(signer)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -34,4 +38,58 @@ func TestPkToken(t *testing.T) {
 	if string(payload) != msg {
 		t.Fatal("OSM payload did not match what we initially wrapped")
 	}
+}
+
+func createPKToken(signer *signer.Signer) (*pktoken.PKToken, error) {
+	cic, err := clientinstance.NewClaims(signer.JWKKey(), map[string]any{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Calculate our nonce from our cic values
+	nonce, err := cic.Commitment()
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate mock id token
+	idToken, err := generateMockIDToken(nonce, "https://github.com/openpubkey", "me")
+	if err != nil {
+		return nil, err
+	}
+
+	// Sign mock id token payload with cic headers
+	cicToken, err := cic.Sign(signer.SigningKey(), signer.JWKKey().Algorithm(), idToken)
+	if err != nil {
+		return nil, err
+	}
+
+	// Combine two tokens into a PK Token
+	return pktoken.New(idToken, cicToken)
+}
+
+func generateMockIDToken(nonce, issuer, audience string) ([]byte, error) {
+	token := openid.New()
+
+	token.Set(`nonce`, nonce)
+
+	// Required token payload values for OpenID
+	token.Set(jwt.IssuerKey, issuer)
+	token.Set(jwt.AudienceKey, audience)
+	token.Set(jwt.IssuedAtKey, time.Now().Unix())
+	token.Set(jwt.ExpirationKey, time.Now().Add(24*time.Hour).Unix())
+	token.Set(jwt.SubjectKey, "1234567890")
+
+	signer, err := signer.NewRSASigner()
+	if err != nil {
+		return nil, err
+	}
+
+	// Sign the token with the secret key
+	signedToken, err := jwt.Sign(token, jwt.WithKey(signer.JWKKey().Algorithm(), signer.SigningKey()))
+	if err != nil {
+		return nil, err
+	}
+
+	return signedToken, nil
 }
