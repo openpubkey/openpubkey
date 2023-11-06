6400a710bb7e6ae497608855d839629af7f10561
lgmugnier <mugnier@bu.edu>
2023-11-06 18:34:41 -0500

    small cleanup but still working

--- parties/webauthn/server.go
+++ parties/webauthn/server.go
@@ -32,22 +32,11 @@ func NewServer() (*Server, error) {
 	}, nil
 }
 
-func (s *Server) Start() error {
-	// Register a new user account
-	http.HandleFunc("/mfa/register-begin", s.beginRegistration)
-	http.HandleFunc("/mfa/register-finish", s.finishRegistration)
-
-	http.HandleFunc("/mfa/login-begin", s.beginLogin)
-	http.HandleFunc("/mfa/login-finish", s.finishLogin)
-	return http.ListenAndServe(":8181", nil)
-}
-
 func (s *Server) Register(user webauthn.User) error {
 	s.user = user
-	// s.server = &http.Server{Addr: ":8080"}
 
-	http.HandleFunc("/beginRegistration", s.beginRegistration)
-	http.HandleFunc("/finishRegistration", s.finishRegistration)
+	http.HandleFunc("/register/begin", s.beginRegistration)
+	http.HandleFunc("/register/finish", s.finishRegistration)
 	http.Handle("/", http.FileServer(http.Dir("../../parties/webauthn/static")))
 
 	fmt.Println("Please open http://localhost:8080")
@@ -72,38 +61,11 @@ func (s *Server) beginRegistration(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("%s", string(optionsJson))
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(optionsJson)
-	fmt.Println("Beginregistration complete")
 }
 
 func (s *Server) finishRegistration(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("starting finishRegistration")
-
-	// // Read the body of the request
-	// body, err := io.ReadAll(r.Body)
-	// if err != nil {
-	// 	// If there is an error reading the body, write an HTTP error response
-	// 	http.Error(w, "Error reading request body", http.StatusInternalServerError)
-	// 	return
-	// }
-	// defer r.Body.Close()
-
-	// // Convert the body to a string and print it
-	// bodyString := string(body)
-	// fmt.Println("Request Body:", bodyString)
-
-	// var ccr protocol.CredentialCreationResponse
-
-	// if err := json.NewDecoder(r.Body).Decode(&ccr); err != nil {
-	// 	fmt.Println("WE FUKCED UP HERE:", err)
-	// }
-
-	// _, err := ccr.Parse()
-	// fmt.Println("ccr ERROR", err)
-
 	credential, err := s.mfa.FinishRegistration(s.user, *s.session, r)
 	if err != nil {
 		fmt.Printf("Failed to finish registration: %+v\n", err)
@@ -115,11 +77,11 @@ func (s *Server) finishRegistration(w http.ResponseWriter, r *http.Request) {
 	// Pseudocode to add the user credential.
 	// s.user.AddCredential(credential)
 	// datastore.SaveUser(user)
-	fmt.Printf("%+v", credential)
+	fmt.Printf("credential: %+v\n", credential)
 
 	w.WriteHeader(201)
 	w.Write([]byte("MFA registration Successful! You may now close this window"))
-	fmt.Println("MFA registration compelte")
+	fmt.Println("MFA registration complete")
 }
 
 func (s *Server) beginLogin(w http.ResponseWriter, r *http.Request) {}
