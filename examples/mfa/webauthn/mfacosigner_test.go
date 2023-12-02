fd3d88e4f4af414f01777e50a5d8981daf07d2a9
Ethan Heilman <ethan.r.heilman@gmail.com>
2023-12-02 11:33:06 -0500

    Breaks Webauthn device into its own struct

--- examples/mfa/webauthn/mfacosigner_test.go
+++ examples/mfa/webauthn/mfacosigner_test.go
@@ -87,42 +87,30 @@ func TestInitAuth(t *testing.T) {
 func TestFullFlow(t *testing.T) {
 
 	// Step 0: Setup
-
-	// Step 0.1: Create our PK Token and signer
-	// Generate the key pair for our cosigner
+	// Create our PK Token and signer
 	alg := jwa.ES256
 	signer, err := util.GenKeyPair(alg)
 	if err != nil {
 		t.Error(err)
 	}
-	upk := signer.Public().(*ecdsa.PublicKey)
-	upkugh := webauthncose.EC2PublicKeyData{
-		PublicKeyData: webauthncose.PublicKeyData{
-			KeyType:   2,
-			Algorithm: int64(webauthncose.AlgES256),
-		},
-		Curve:  int64(webauthncose.AlgES256),
-		XCoord: upk.X.Bytes(),
-		YCoord: upk.Y.Bytes(),
-	}
-	upkBytes, err := webauthncbor.Marshal(upkugh)
-	if err != nil {
-		t.Error(err)
-	}
-
 	email := "arthur.aardvark@example.com"
 	pkt, err := mocks.GenerateMockPKTokenWithEmail(signer, alg, email)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Step 0.2: Create our MFA Cosigner
+	// Create our MFA Cosigner
 	cosSigner, err := util.GenKeyPair(alg)
 	kid := "test-kid"
 	cosignerURI := "https://example.com/mfacosigner"
 	RPID := "http://localhost"
-	rpIDHash := sha256.Sum256([]byte(RPID))
 	cos, err := NewCosigner(cosSigner, alg, cosignerURI, kid, RPID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Create our MFA device
+	wauthnDevice, err := NewWebauthnDevice(RPID)
 	if err != nil {
 		t.Error(err)
 	}
@@ -144,47 +132,16 @@ func TestFullFlow(t *testing.T) {
 	}
 
 	// Step 2: Register MFA device
-	credCreation, err := cos.BeginRegistration(authID)
+	createCreation, err := cos.BeginRegistration(authID)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if credCreation == nil {
+	if createCreation == nil {
 		t.Fatal("Expected cred creation to not be nil")
 	}
-	credCreationResp := &protocol.ParsedCredentialCreationData{
-		ParsedPublicKeyCredential: protocol.ParsedPublicKeyCredential{
-			ParsedCredential: protocol.ParsedCredential{
-				ID:   "6xrtBhJQW6QU4tOaB4rrHaS2Ks0yDDL_q8jDC16DEjZ-VLVf4kCRkvl2xp2D71sTPYns-exsHQHTy3G-zJRK8g",
-				Type: "public-key",
-			},
-			RawID: []byte{1, 1, 1, 1, 1},
-			ClientExtensionResults: protocol.AuthenticationExtensionsClientOutputs{
-				"appid": true,
-			},
-			AuthenticatorAttachment: "platform",
-		},
-		Response: protocol.ParsedAttestationResponse{
-			CollectedClientData: protocol.CollectedClientData{
-				Type:      protocol.CeremonyType("webauthn.create"),
-				Challenge: credCreation.Response.Challenge.String(),
-				Origin:    credCreation.Response.RelyingParty.ID,
-			},
-			AttestationObject: protocol.AttestationObject{
-				Format:      "none",
-				RawAuthData: []byte{1, 1, 1, 1, 1},
-				AuthData: protocol.AuthenticatorData{
-					RPIDHash: rpIDHash[:],
-					Counter:  0,
-					Flags:    0x041,
-					AttData: protocol.AttestedCredentialData{
-						AAGUID:              make([]byte, 16),
-						CredentialID:        []byte{5, 1, 1, 1, 1},
-						CredentialPublicKey: upkBytes,
-					},
-				},
-			},
-			Transports: []protocol.AuthenticatorTransport{protocol.USB, protocol.NFC, "fake"},
-		},
+	credCreationResp, err := wauthnDevice.RegResp(createCreation)
+	if err != nil {
+		t.Fatal(err)
 	}
 	err = cos.FinishRegistration(authID, credCreationResp)
 	if err != nil {
@@ -193,55 +150,13 @@ func TestFullFlow(t *testing.T) {
 
 	// Step 3: Login MFA device
 	credAssert, err := cos.BeginLogin(authID)
-	loginResp := &protocol.ParsedCredentialAssertionData{
-		ParsedPublicKeyCredential: protocol.ParsedPublicKeyCredential{
-			ParsedCredential: protocol.ParsedCredential{
-				ID:   "AI7D5q2P0LS-Fal9ZT7CHM2N5BLbUunF92T8b6iYC199bO2kagSuU05-5dZGqb1SP0A0lyTWng",
-				Type: "public-key",
-			},
-			RawID: []byte{5, 1, 1, 1, 1},
-			ClientExtensionResults: map[string]interface{}{
-				"appID": "example.com",
-			},
-		},
-		Response: protocol.ParsedAssertionResponse{
-			CollectedClientData: protocol.CollectedClientData{
-				Type:      protocol.CeremonyType("webauthn.get"),
-				Challenge: credAssert.Response.Challenge.String(),
-				Origin:    RPID,
-				Hint:      "do not compare clientDataJSON against a template. See https://goo.gl/yabPex",
-			},
-			AuthenticatorData: protocol.AuthenticatorData{
-				RPIDHash: rpIDHash[:],
-				Counter:  1553097241,
-				Flags:    0x045,
-				AttData: protocol.AttestedCredentialData{
-					AAGUID:              []byte{1, 1, 1, 1, 3},
-					CredentialID:        []byte{1, 1, 1, 1, 4},
-					CredentialPublicKey: []byte{1, 1, 1, 1, 5},
-				},
-			},
-			Signature:  []byte{1, 1, 1, 1, 6},
-			UserHandle: []byte("1234567890"), // ID Token sub
-		},
-		Raw: protocol.CredentialAssertionResponse{
-			PublicKeyCredential: protocol.PublicKeyCredential{
-				Credential: protocol.Credential{
-					Type: "public-key",
-					ID:   "AI7D5q2P0LS-Fal9ZT7CHM2N5BLbUunF92T8b6iYC199bO2kagSuU05-5dZGqb1SP0A0lyTWng",
-				},
-				RawID: []byte{5, 1, 1, 1, 1},
-				ClientExtensionResults: map[string]interface{}{
-					"appID": "example.com",
-				},
-			},
-		},
+	if err != nil {
+		t.Fatal(err)
 	}
-	clientDataHash := sha256.Sum256(loginResp.Raw.AssertionResponse.ClientDataJSON)
-	sigData := append(loginResp.Raw.AssertionResponse.AuthenticatorData, clientDataHash[:]...)
-	sigHash := sha256.Sum256(sigData)
-	sigWebauthn, err := signer.Sign(rand.Reader, sigHash[:], crypto.SHA256)
-	loginResp.Response.Signature = sigWebauthn
+	loginResp, err := wauthnDevice.LoginResp(credAssert)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if err != nil {
 		t.Fatal(err)
@@ -278,4 +193,144 @@ func TestFullFlow(t *testing.T) {
 	if pktCos.Cos == nil {
 		t.Fatal("Expected pktCos to be cosigned")
 	}
+}
+
+// For testing purposes we create a WebAuthn device to run the client part of the protocol
+type WebAuthnDevice struct {
+	signer     crypto.Signer
+	PubkeyCbor []byte
+	RpID       string
+	RpIDHash   []byte
+}
+
+func NewWebauthnDevice(rpID string) (*WebAuthnDevice, error) {
+	alg := jwa.ES256
+	signer, err := util.GenKeyPair(alg)
+	if err != nil {
+		return nil, err
+	}
+
+	pubkey := signer.Public().(*ecdsa.PublicKey)
+	pubkeyCbor := webauthncose.EC2PublicKeyData{
+		PublicKeyData: webauthncose.PublicKeyData{
+			KeyType:   int64(webauthncose.EllipticKey),
+			Algorithm: int64(webauthncose.AlgES256),
+		},
+		Curve:  int64(webauthncose.AlgES256),
+		XCoord: pubkey.X.Bytes(),
+		YCoord: pubkey.Y.Bytes(),
+	}
+	pubkeyCborBytes, err := webauthncbor.Marshal(pubkeyCbor)
+	if err != nil {
+		return nil, err
+	}
+
+	rpIDHash := sha256.Sum256([]byte(rpID))
+
+	return &WebAuthnDevice{
+		signer:     signer,
+		PubkeyCbor: pubkeyCborBytes,
+		RpID:       rpID,
+		RpIDHash:   rpIDHash[:],
+	}, nil
+}
+
+func (wa *WebAuthnDevice) RegResp(createCreation *protocol.CredentialCreation) (*protocol.ParsedCredentialCreationData, error) {
+	return &protocol.ParsedCredentialCreationData{
+		ParsedPublicKeyCredential: protocol.ParsedPublicKeyCredential{
+			ParsedCredential: protocol.ParsedCredential{
+				ID:   "6xrtBhJQW6QU4tOaB4rrHaS2Ks0yDDL_q8jDC16DEjZ-VLVf4kCRkvl2xp2D71sTPYns-exsHQHTy3G-zJRK8g",
+				Type: "public-key",
+			},
+			RawID: []byte{1, 1, 1, 1, 1},
+			ClientExtensionResults: protocol.AuthenticationExtensionsClientOutputs{
+				"appid": true,
+			},
+			AuthenticatorAttachment: "platform",
+		},
+		Response: protocol.ParsedAttestationResponse{
+			CollectedClientData: protocol.CollectedClientData{
+				Type:      protocol.CeremonyType("webauthn.create"),
+				Challenge: createCreation.Response.Challenge.String(),
+				Origin:    createCreation.Response.RelyingParty.ID,
+			},
+			AttestationObject: protocol.AttestationObject{
+				Format:      "none",
+				RawAuthData: []byte{1, 1, 1, 1, 1},
+				AuthData: protocol.AuthenticatorData{
+					RPIDHash: wa.RpIDHash,
+					Counter:  0,
+					Flags:    0x041,
+					AttData: protocol.AttestedCredentialData{
+						AAGUID:              make([]byte, 16),
+						CredentialID:        []byte{5, 1, 1, 1, 1},
+						CredentialPublicKey: wa.PubkeyCbor,
+					},
+				},
+			},
+			Transports: []protocol.AuthenticatorTransport{protocol.USB, protocol.NFC, "fake"},
+		},
+	}, nil
+}
+
+func (wa *WebAuthnDevice) LoginResp(credAssert *protocol.CredentialAssertion) (*protocol.ParsedCredentialAssertionData, error) {
+	loginRespData := &protocol.ParsedCredentialAssertionData{
+		ParsedPublicKeyCredential: protocol.ParsedPublicKeyCredential{
+			ParsedCredential: protocol.ParsedCredential{
+				ID:   "AI7D5q2P0LS-Fal9ZT7CHM2N5BLbUunF92T8b6iYC199bO2kagSuU05-5dZGqb1SP0A0lyTWng",
+				Type: "public-key",
+			},
+			RawID: []byte{5, 1, 1, 1, 1},
+			ClientExtensionResults: map[string]interface{}{
+				"appID": "example.com",
+			},
+		},
+		Response: protocol.ParsedAssertionResponse{
+			CollectedClientData: protocol.CollectedClientData{
+				Type:      protocol.CeremonyType("webauthn.get"),
+				Challenge: credAssert.Response.Challenge.String(),
+				Origin:    wa.RpID,
+				Hint:      "do not compare clientDataJSON against a template. See https://goo.gl/yabPex",
+			},
+			AuthenticatorData: protocol.AuthenticatorData{
+				RPIDHash: wa.RpIDHash,
+				Counter:  1553097241,
+				Flags:    0x045,
+				AttData: protocol.AttestedCredentialData{
+					AAGUID:              []byte{1, 1, 1, 1, 3},
+					CredentialID:        []byte{1, 1, 1, 1, 4},
+					CredentialPublicKey: []byte{1, 1, 1, 1, 5},
+				},
+			},
+			Signature:  []byte{1, 1, 1, 1, 6},
+			UserHandle: []byte("1234567890"), // ID Token sub
+		},
+		Raw: protocol.CredentialAssertionResponse{
+			PublicKeyCredential: protocol.PublicKeyCredential{
+				Credential: protocol.Credential{
+					Type: "public-key",
+					ID:   "AI7D5q2P0LS-Fal9ZT7CHM2N5BLbUunF92T8b6iYC199bO2kagSuU05-5dZGqb1SP0A0lyTWng",
+				},
+				RawID: []byte{5, 1, 1, 1, 1},
+				ClientExtensionResults: map[string]interface{}{
+					"appID": "example.com",
+				},
+			},
+		},
+	}
+
+	return wa.SignLoginChallenge(loginRespData)
+}
+
+func (wa *WebAuthnDevice) SignLoginChallenge(loginRespData *protocol.ParsedCredentialAssertionData) (*protocol.ParsedCredentialAssertionData, error) {
+	clientDataHash := sha256.Sum256(loginRespData.Raw.AssertionResponse.ClientDataJSON)
+	sigData := append(loginRespData.Raw.AssertionResponse.AuthenticatorData, clientDataHash[:]...)
+	sigHash := sha256.Sum256(sigData)
+	sigWebauthn, err := wa.signer.Sign(rand.Reader, sigHash[:], crypto.SHA256)
+	if err != nil {
+		return nil, err
+	}
+	loginRespData.Response.Signature = sigWebauthn
+
+	return loginRespData, nil
 }
