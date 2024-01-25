134aafed0a3982348e4456ce02e2696db472a7e9
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-01-25 17:51:13 -0500

    Changing clientid for MFA example

--- examples/mfa/example.go
+++ examples/mfa/example.go
@@ -29,9 +29,9 @@ import (
 )
 
 var (
-	clientID = "3249310342-hkmqe7rncgqruetb0ibddoob5esl1jje.apps.googleusercontent.com"
+	clientID = "992028499768-ce9juclb3vvckh23r83fjkmvf1lvjq18.apps.googleusercontent.com"
 	// The clientSecret was intentionally checked in. It holds no power and is used for development. Do not report as a security issue
-	clientSecret = "uZIaSqmTjhJH0MWPOByoj0Es" // Google requires a ClientSecret even if this a public OIDC App
+	clientSecret = "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" // Google requires a ClientSecret even if this a public OIDC App
 	scopes       = []string{"openid profile email"}
 	redirURIPort = "3000"
 	callbackPath = "/login-callback"
