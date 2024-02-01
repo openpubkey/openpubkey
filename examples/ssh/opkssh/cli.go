02dbac9a36c7f7e02f41e242386e7e0056e6a957
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-02-01 12:11:33 -0500

    Adds the approved openpubkey Google clientid to all examples (#95)

--- examples/ssh/opkssh/cli.go
+++ examples/ssh/opkssh/cli.go
@@ -36,11 +36,9 @@ import (
 )
 
 var (
-	key              = []byte("NotASecureKey123")
-	clientID         = "184968138938-g1fddl5tglo7mnlbdak8hbsqhhf79f32.apps.googleusercontent.com"
-	requiredAudience = "184968138938-g1fddl5tglo7mnlbdak8hbsqhhf79f32.apps.googleusercontent.com"
-	// The clientSecret was intentionally checked in for the purposes of this example,. It holds no power. Do not report as a security issue
-	clientSecret = "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" // Google requires a ClientSecret even if this a public OIDC App
+	clientID = "992028499768-ce9juclb3vvckh23r83fjkmvf1lvjq18.apps.googleusercontent.com"
+	// The clientSecret was intentionally checked in. It holds no power and is used for development. Do not report as a security issue
+	clientSecret = "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" // Google requires a ClientSecret even if this a public OIDC App
 	scopes       = []string{"openid profile email"}
 	redirURIPort = "3000"
 	callbackPath = "/login-callback"
