128a76c6f7a19d0f686b849bcd1272969fa09947
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-02-03 19:46:48 -0500

    Moves ca package into examples

--- cert/cert.go
+++ cert/cert.go
@@ -1,3 +1,19 @@
+// Copyright 2024 OpenPubkey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package cert
 
 import (
@@ -5,12 +21,14 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/asn1"
 	"encoding/json"
 	"encoding/pem"
 	"fmt"
 	"math/big"
 	"time"
 
+	"github.com/openpubkey/openpubkey/client"
 	"github.com/openpubkey/openpubkey/pktoken"
 )
 
@@ -19,44 +37,63 @@ import (
 //   - User public key is mapped to the RawSubjectPublicKeyInfo field
 //   - Raw PK token is mapped to the SubjectKeyId field
 func CreateX509Cert(pkToken *pktoken.PKToken, signer crypto.Signer) ([]byte, error) {
-	pkTokenJSON, err := json.Marshal(pkToken)
+	template, err := PktToX509Template(pkToken)
+
+	// create a self-signed X.509 certificate
+	certDER, err := x509.CreateCertificate(rand.Reader, template, template, signer.Public(), signer)
+	if err != nil {
+		return nil, fmt.Errorf("error creating X.509 certificate: %w", err)
+	}
+	certBlock := &pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+	return pem.EncodeToMemory(certBlock), nil
+}
+
+func PktToX509Template(pkt *pktoken.PKToken) (*x509.Certificate, error) {
+	pktJson, err := json.Marshal(pkt)
 	if err != nil {
 		return nil, fmt.Errorf("error marshalling PK token to JSON: %w", err)
 	}
 
-	// get subject identitifer from pk token
-	var payload struct {
-		Subject string `json:"sub"`
+	// get subject identifier from pk token
+	idtClaims := new(client.OidcClaims)
+	if err := json.Unmarshal(pkt.Payload, idtClaims); err != nil {
+		return nil, err
 	}
-	if err := json.Unmarshal(pkToken.Payload, &payload); err != nil {
+
+	cic, err := pkt.GetCicValues()
+	if err != nil {
+		return nil, err
+	}
+	upk := cic.PublicKey()
+	var rawkey interface{} // This is the raw key, like *rsa.PrivateKey or *ecdsa.PrivateKey
+	if err := upk.Raw(&rawkey); err != nil {
 		return nil, err
 	}
 
 	// encode ephemeral public key
-	ecPub, err := x509.MarshalPKIXPublicKey(signer.Public())
+	ecPub, err := x509.MarshalPKIXPublicKey(rawkey)
 	if err != nil {
 		return nil, fmt.Errorf("error marshalling public key: %w", err)
 	}
-
-	template := x509.Certificate{
+	template := &x509.Certificate{
 		SerialNumber:            big.NewInt(1),
-		Subject:                 pkix.Name{CommonName: payload.Subject},
+		Subject:                 pkix.Name{CommonName: idtClaims.Subject},
 		RawSubjectPublicKeyInfo: ecPub,
 		NotBefore:               time.Now(),
 		NotAfter:                time.Now().Add(365 * 24 * time.Hour), // valid for 1 year
 		KeyUsage:                x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:             []x509.ExtKeyUsage{x509.ExtKeyUsageCodeSigning},
 		BasicConstraintsValid:   true,
-		DNSNames:                []string{payload.Subject},
+		DNSNames:                []string{idtClaims.Subject},
 		IsCA:                    false,
-		SubjectKeyId:            pkTokenJSON,
+		ExtraExtensions: []pkix.Extension{{
+			// OID for OIDC Issuer extension
+			Id:       asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 57264, 1, 1},
+			Critical: false,
+			Value:    []byte(idtClaims.Issuer),
+		}},
+		SubjectKeyId: pktJson,
 	}
 
-	// create a self-signed X.509 certificate
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, signer.Public(), signer)
-	if err != nil {
-		return nil, fmt.Errorf("error creating X.509 certificate: %w", err)
-	}
-	certBlock := &pem.Block{Type: "CERTIFICATE", Bytes: certDER}
-	return pem.EncodeToMemory(certBlock), nil
+	return template, nil
 }
---
+++ examples/x509/example.go
@@ -0,0 +1,224 @@
+// Copyright 2024 OpenPubkey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/awnumar/memguard"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+
+	"github.com/openpubkey/openpubkey/client"
+	"github.com/openpubkey/openpubkey/client/providers"
+	"github.com/openpubkey/openpubkey/examples/x509/ca"
+	"github.com/openpubkey/openpubkey/pktoken"
+	"github.com/openpubkey/openpubkey/util"
+	"golang.org/x/crypto/sha3"
+)
+
+// Variables for building our google provider
+var (
+	clientID = "184968138938-g1fddl5tglo7mnlbdak8hbsqhhf79f32.apps.googleusercontent.com"
+	// The clientSecret was intentionally checked in for the purposes of this example,. It holds no power. Do not report as a security issue
+	clientSecret = "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" // Google requires a ClientSecret even if this a public OIDC App
+	scopes       = []string{"openid profile email"}
+	redirURIPort = "3000"
+	callbackPath = "/login-callback"
+	redirectURI  = fmt.Sprintf("http://localhost:%v%v", redirURIPort, callbackPath)
+
+	// File names for when we save or load our pktoken and the corresponding signing key
+	skFileName  = "key.pem"
+	pktFileName = "pktoken.json"
+)
+
+func main() {
+	// Safely terminate in case of an interrupt signal
+	memguard.CatchInterrupt()
+
+	// Purge the session when we return
+	defer memguard.Purge()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("OpenPubkey: command choices are login, sign, and cert")
+		return
+	}
+
+	signGQ := true
+	keyAlgorithm := jwa.ES256
+
+	// Directory for saving data
+	outputDir := "output/google"
+
+	command := os.Args[1]
+	switch command {
+	case "login":
+		if err := login(outputDir, keyAlgorithm, signGQ); err != nil {
+			fmt.Println("Error logging in:", err)
+		} else {
+			fmt.Println("Login successful!")
+		}
+	case "sign":
+		message := "sign me!!"
+		if err := sign(message, outputDir, keyAlgorithm, signGQ); err != nil {
+			fmt.Println("Failed to sign test message:", err)
+		}
+	default:
+		fmt.Println("Unrecognized command:", command)
+	}
+}
+
+func CA(pkt *pktoken.PKToken) error {
+
+	return nil
+}
+
+func login(outputDir string, alg jwa.KeyAlgorithm, signGQ bool) error {
+	signer, err := util.GenKeyPair(alg)
+	if err != nil {
+		return err
+	}
+
+	op := &providers.GoogleOp{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       scopes,
+		RedirURIPort: redirURIPort,
+		CallbackPath: callbackPath,
+		RedirectURI:  redirectURI,
+	}
+
+	client := &client.OpkClient{
+		Op: op,
+	}
+
+	pkt, err := client.Auth(context.Background(), signer, alg, map[string]any{"extra": "yes"}, signGQ)
+	if err != nil {
+		return err
+	}
+
+	// Pretty print our json token
+	pktJson, err := json.MarshalIndent(pkt, "", "  ")
+	if err != nil {
+		return err
+	}
+	CertAuth, err := ca.New(op)
+	if err != nil {
+		return err
+	}
+
+	pemSubCert, err := CertAuth.PktToSignedX509(pktJson)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Issued Cert: \n", string(pemSubCert))
+
+	err = CertAuth.VerifyPktCert(pemSubCert)
+	if err != nil {
+		return err
+	}
+
+	msg := []byte("All is discovered - flee at once")
+	signedMsg, err := pkt.NewSignedMessage(msg, signer)
+	if err != nil {
+		return err
+	}
+	println(string(msg))
+	println(string(signedMsg))
+
+	return nil
+}
+
+func sign(message string, outputDir string, alg jwa.KeyAlgorithm, signGq bool) error {
+	signer, pkt, err := loadLogin(outputDir)
+	if err != nil {
+		return fmt.Errorf("failed to load client state: %w", err)
+	}
+
+	msgHashSum := sha3.Sum256([]byte(message))
+	sig, err := signer.Sign(rand.Reader, msgHashSum[:], crypto.SHA256)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Signed Message:", message)
+	fmt.Println("Praise Sigma:", base64.StdEncoding.EncodeToString(sig))
+	fmt.Println("Hash:", hex.EncodeToString(msgHashSum[:]))
+	fmt.Println("Cert:")
+
+	pktJson, err := json.Marshal(pkt)
+	if err != nil {
+		return err
+	}
+
+	// Pretty print our json token
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, pktJson, "", "  "); err != nil {
+		return err
+	}
+	fmt.Println(prettyJSON.String())
+
+	return nil
+}
+
+func saveLogin(outputDir string, sk *ecdsa.PrivateKey, pkt *pktoken.PKToken) error {
+	if err := os.MkdirAll(outputDir, 0777); err != nil {
+		return err
+	}
+
+	skFilePath := path.Join(outputDir, skFileName)
+	if err := util.WriteSKFile(skFilePath, sk); err != nil {
+		return err
+	}
+
+	pktFilePath := path.Join(outputDir, pktFileName)
+	pktJson, err := json.Marshal(pkt)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(pktFilePath, pktJson, 0600)
+}
+
+func loadLogin(outputDir string) (crypto.Signer, *pktoken.PKToken, error) {
+	skFilePath := path.Join(outputDir, skFileName)
+	key, err := util.ReadSKFile(skFilePath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pktFilePath := path.Join(outputDir, pktFileName)
+	pktJson, err := os.ReadFile(pktFilePath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var pkt *pktoken.PKToken
+	if err := json.Unmarshal(pktJson, &pkt); err != nil {
+		return nil, nil, err
+	}
+
+	return key, pkt, nil
+}
