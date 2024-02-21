69b842d8b08ff5a8c6be1b0959aa601c75497998
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-02-20 21:38:50 -0500

    Comments out test that downloads full JWKS archive from sui full node

--- examples/sui/reader/reader_test.go
+++ examples/sui/reader/reader_test.go
@@ -1,9 +1,6 @@
 package reader
 
 import (
-	"context"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/openpubkey/openpubkey/ajwks"
@@ -15,45 +12,45 @@ func TestReader(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestRequest(t *testing.T) {
+// func TestRequest(t *testing.T) {
 
-	suiClient := NewSuiJwksArchive("https://fullnode.mainnet.sui.io:443")
-	jwksMap, err := suiClient.GetLatestJwks(context.TODO())
-	require.NoError(t, err)
+// 	suiClient := NewSuiJwksArchive("https://fullnode.mainnet.sui.io:443")
+// 	jwksMap, err := suiClient.GetLatestJwks(context.TODO())
+// 	require.NoError(t, err)
 
-	for iss, jwks := range jwksMap {
-		println(iss, len(jwks))
-	}
+// 	for iss, jwks := range jwksMap {
+// 		println(iss, len(jwks))
+// 	}
 
-	depth := 350
-	jwksMapPast, err := suiClient.GetPastJwks(context.TODO(), depth)
-	require.NoError(t, err)
-	require.NotNil(t, jwksMapPast)
+// 	depth := 350
+// 	jwksMapPast, err := suiClient.GetPastJwks(context.TODO(), depth)
+// 	require.NoError(t, err)
+// 	require.NotNil(t, jwksMapPast)
 
-	for iss, jwksList := range jwksMapPast {
-		println(iss)
-		for _, jwkData := range *jwksList {
-			println("\t", jwkData.CreateTime, jwkData.Epoch)
-		}
-	}
+// 	for iss, jwksList := range jwksMapPast {
+// 		println(iss)
+// 		for _, jwkData := range *jwksList {
+// 			println("\t", jwkData.CreateTime, jwkData.Epoch)
+// 		}
+// 	}
 
-	for iss, jwksList := range jwksMapPast {
-		aJwks := ajwks.New(iss)
-		for _, save := range *jwksList {
+// 	for iss, jwksList := range jwksMapPast {
+// 		aJwks := ajwks.New(iss)
+// 		for _, save := range *jwksList {
 
-			err = aJwks.AddJwksSave(save)
-			if err != nil {
-				require.NoError(t, err)
-			}
-		}
-		aJwks.Print()
-		j, err := aJwks.GetSaves()
-		require.NoError(t, err)
-		fmt.Println(string(j))
-		err = aJwks.SaveToFile("jwks-" + strings.Split(aJwks.Issuer, ".")[1] + "-" + "350" + ".json")
-		require.NoError(t, err)
-	}
-}
+// 			err = aJwks.AddJwksSave(save)
+// 			if err != nil {
+// 				require.NoError(t, err)
+// 			}
+// 		}
+// 		aJwks.Print()
+// 		j, err := aJwks.GetSaves()
+// 		require.NoError(t, err)
+// 		fmt.Println(string(j))
+// 		err = aJwks.SaveToFile("jwks-" + strings.Split(aJwks.Issuer, ".")[1] + "-" + "350" + ".json")
+// 		require.NoError(t, err)
+// 	}
+// }
 
 func TestRead(t *testing.T) {
 	archive, err := ajwks.NewFromFile("jwks-google-10.json")
