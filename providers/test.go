c6fc1a97661b120df9e385e736fafe7839bcc1eb
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-04-01 12:42:06 -0400

    Adds comment about issue #162

--- providers/test.go
+++ providers/test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TODO: This code is duplicated in mocks to avoid circular dependencies. When we solve this circular dependencies remove this and point to mocks.
+// Tracked in: https://github.com/openpubkey/openpubkey/issues/162
 func genCIC(t *testing.T) *clientinstance.Claims {
 	alg := jwa.ES256
 	signer, err := util.GenKeyPair(alg)
