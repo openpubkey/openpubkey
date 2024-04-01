4127cdd9c8b82ca137b0277e8ac3f4983618fa23
Ethan Heilman <ethan.r.heilman@gmail.com>
2024-04-01 14:06:09 -0400

    Removes unused function definitions

--- providers/op.go
+++ providers/op.go
@@ -2,12 +2,10 @@ package providers
 
 import (
 	"context"
-	"crypto"
 	"fmt"
 	"net/http"
 	"os"
 
-	"github.com/lestrrat-go/jwx/v2/jws"
 	"github.com/openpubkey/openpubkey/discover"
 	"github.com/openpubkey/openpubkey/pktoken"
 	"github.com/openpubkey/openpubkey/pktoken/clientinstance"
@@ -28,14 +26,6 @@ type BrowserOpenIdProvider interface {
 	OpenIdProvider
 	HookHTTPSession(h http.HandlerFunc)
 }
-
-// We use this to inject our own publicKeyFunc for testing and extensibility.
-// For an example of how this works see github_actions_test
-type PublicKeyFunc = func(ctx context.Context, headers jws.Headers) (crypto.PublicKey, error)
-
-// We use this to inject our own publicKeyFunc for testing and extensibility.
-// For an example of how this works see github_actions_test
-type JwksGetFunc = func(ctx context.Context, issuer string) ([]byte, error)
 
 func getEnvVar(name string) (string, error) {
 	value, ok := os.LookupEnv(name)
