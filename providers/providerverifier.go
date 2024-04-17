2a5ffa17e9d9aa57b1d5630d35a39a68e0ec1989
hcrypt <95130644+thebigbone@users.noreply.github.com>
2024-04-17 20:56:42 +0530

    bugfix: fixes #185 by checking algorithm from public key rather token (#188)
    * SPDX-License-Identifier: Apache-2.0. fix #185
    
    ---------
    
    Signed-off-by: thebigbone <pacman@duck.com>

--- providers/providerverifier.go
+++ providers/providerverifier.go
@@ -132,6 +132,12 @@ func (v *DefaultProviderVerifier) VerifyIDToken(ctx context.Context, idToken []b
 			return fmt.Errorf("failed to get OP public key: %w", err)
 		}
 
+		// Ensure that the algorithm of public key from OpenID Provider matches the algorithm specified in the ID Token
+		_, ok := pubKeyRecord.PublicKey.(*rsa.PublicKey)
+		if !ok {
+			return fmt.Errorf("public key is not an RSA public key")
+		}
+
 		if _, err := jws.Verify(idToken, jws.WithKey(alg, pubKeyRecord.PublicKey)); err != nil {
 			return err
 		}
