ccc5823e57ba62957dff15427fea070f98a8cfc4
thebigbone <pacman@duck.com>
2024-05-11 12:34:04 +0530

    use testify

--- providers/op_test.go
+++ providers/op_test.go
@@ -3,6 +3,8 @@ package providers
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetEnvVar(t *testing.T) {
@@ -10,16 +12,9 @@ func TestGetEnvVar(t *testing.T) {
 	os.Setenv("TEST_ENV_VAR", "test value")
 	defer os.Setenv("TEST_ENV_VAR", originalEnv)
 
-	value, err := getEnvVar("TEST_ENV_VAR")
-	if err != nil {
-		t.Errorf("getEnvVar returned an error: %v", err)
-	}
-	if value != "test value" {
-		t.Errorf("getEnvVar returned unexpected value: %s", value)
-	}
+	_, err := getEnvVar("TEST_ENV_VAR")
+	require.NoError(t, err)
 
 	_, err = getEnvVar("NON_EXISTENT_ENV_VAR")
-	if err == nil {
-		t.Error("getEnvVar did not return an error for non-existent environment variable")
-	}
+	require.Error(t, err)
 }
