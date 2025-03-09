6db613d807dfb33e19e5d5a53ed84b6c26235bd9
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-09 17:14:48 -0400

    Sets boolean that lets us load user's policy via a sudoer script

--- opkssh/commands/verify.go
+++ opkssh/commands/verify.go
@@ -96,6 +96,7 @@ func OpkPolicyEnforcerFunc(username string) PolicyEnforcerFunc {
 		PolicyLoader: &policy.UserMultiFileLoader{
 			UserPolicyLoader: policy.NewUserFileLoader(),
 			Username:         username,
+			LoadWithScript:   true, // This is needed to load policy from the user's home directory
 		},
 	}
 	return policyEnforcer.CheckPolicy
