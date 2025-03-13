9f90643971d13c3fdba13c375c3a3a545fac6873
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-13 15:40:02 -0400

    Removes regex from sudoer config

--- opkssh/scripts/install-linux.sh
+++ opkssh/scripts/install-linux.sh
@@ -135,7 +135,7 @@ if command -v $BINARY_NAME &> /dev/null; then
     fi
 
     # Sudo regex support was added in 1.9.10. If you are using an older version of sudoer home policy will not work unless you set the AuthroizedKeysCommand user to root
-    SUDOERS_RULE_READ_HOME="$AUTH_CMD_USER ALL=(ALL) NOPASSWD: /usr/local/bin/opkssh readhome ^[a-zA-Z0-9_-]+$"
+    SUDOERS_RULE_READ_HOME="$AUTH_CMD_USER ALL=(ALL) NOPASSWD: /usr/local/bin/opkssh readhome *"
     if ! sudo grep -qxF "$SUDOERS_RULE_READ_HOME" /etc/sudoers; then
         echo "Adding sudoers rule for $AUTH_CMD_USER..."
         echo "$SUDOERS_RULE_READ_HOME" | sudo tee -a /etc/sudoers > /dev/null
