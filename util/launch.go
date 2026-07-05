3543e6b1d85ad1136033a95b34e4b6874a4774ae
sukujgrg <sukujgrg@gmail.com>
2026-07-06 07:23:30 +1000

    fix: launch browser via PowerShell on Windows/WSL, safely encoded to avoid injection

--- util/launch.go
+++ util/launch.go
@@ -17,15 +17,18 @@
 package util
 
 import (
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+	"unicode/utf16"
 )
 
 // https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
 // open opens the specified URL in the default browser of the user.
-// url must be trusted: on Windows/WSL it is passed to powershell.exe -Command, which is vulnerable to injection if url contains untrusted input.
 func OpenUrl(url string) error {
 	switch runtime.GOOS {
 	case "windows":
@@ -42,14 +45,24 @@ func OpenUrl(url string) error {
 	}
 }
 
+// openWithPowerShell launches url via PowerShell using -EncodedCommand.
+// The URL is embedded as a PowerShell single-quoted string literal with
+// embedded single quotes escaped. This avoids injection through PowerShell
+// metacharacters in the URL. -EncodedCommand is used instead of -Command
+// because -Command re-joins and re-parses all trailing argv entries as a
+// single script, which would otherwise require additional care to avoid
+// argv-splitting ambiguity.
 func openWithPowerShell(url string) error {
-	return exec.Command(
-		"powershell.exe",
-		"-NoProfile",
-		"-Command",
-		"Start-Process",
-		url,
-	).Start()
+	script := fmt.Sprintf("Start-Process '%s'", strings.ReplaceAll(url, "'", "''"))
+
+	u16 := utf16.Encode([]rune(script))
+	buf := make([]byte, len(u16)*2)
+	for i, r := range u16 {
+		binary.LittleEndian.PutUint16(buf[i*2:], r)
+	}
+	encoded := base64.StdEncoding.EncodeToString(buf)
+
+	return exec.Command("powershell.exe", "-NoProfile", "-EncodedCommand", encoded).Start()
 }
 
 func isWSL() bool {
